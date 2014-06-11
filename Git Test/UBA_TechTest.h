bdc25a373ba344de8bcf868170714b754d6d0a3f
UBA Tech <bbeswick@Bryans-Mac-mini.local>
2014-06-11 14:22:41 -0400

    added Test class

---
+++ Git Test/UBA_TechTest.h
@@ -0,0 +1,13 @@
+//
+//  UBA_TechTest.h
+//  Git Test
+//
+//  Created by UBA Tech on 6/11/14.
+//  Copyright (c) 2014 UBA Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UBA_TechTest : NSObject
+
+@end
---
+++ Git Test/UBA_TechTest.m
@@ -0,0 +1,13 @@
+//
+//  UBA_TechTest.m
+//  Git Test
+//
+//  Created by UBA Tech on 6/11/14.
+//  Copyright (c) 2014 UBA Tech. All rights reserved.
+//
+
+#import "UBA_TechTest.h"
+
+@implementation UBA_TechTest
+
+@end
